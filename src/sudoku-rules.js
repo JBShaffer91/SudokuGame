6a9ad1eda069be1531839c44dd58ed90eb5e8350
Justin Shaffer <justinbshaffer91@gmail.com>
2023-02-27 20:14:41 -0800

    add passing test for checkSubgrid() function

--- src/sudoku-rules.js
+++ src/sudoku-rules.js
@@ -3,8 +3,6 @@ function checkRow(row) {
   return set.size === row.length && !set.has(0);
 }
 
-module.exports = { checkRow };
-
 function checkColumn(grid, columnIndex) {
   const column = [];
   for (let i = 0; i < grid.length; i++) {
@@ -14,4 +12,23 @@ function checkColumn(grid, columnIndex) {
   return set.size === column.length && !set.has(0);
 }
 
-module.exports = { checkRow, checkColumn };
+function checkSubgrid(grid, row, col, num) {
+  const subgridRowStart = Math.floor(row / 3) * 3;
+  const subgridColStart = Math.floor(col / 3) * 3;
+  const subgridValues = new Set();
+
+  for (let i = subgridRowStart; i < subgridRowStart + 3; i++) {
+    for (let j = subgridColStart; j < subgridColStart + 3; j++) {
+      const value = grid[i][j];
+      if (subgridValues.has(value)) {
+        return false;
+      }
+      subgridValues.add(value);
+    }
+  }
+
+  return true;
+}
+
+
+module.exports = { checkRow, checkColumn, checkSubgrid };
--- test/sudoku-rules.test.js
+++ test/sudoku-rules.test.js
@@ -1,5 +1,6 @@
 const { checkRow } = require('../src/sudoku-rules');
 const { checkColumn } = require('../src/sudoku-rules');
+const { checkSubgrid } = require('../src/sudoku-rules');
 
 describe('checkRow', () => {
   it('should return true if the row is valid', () => {
@@ -15,7 +16,8 @@ describe('checkRow', () => {
 
 describe('checkColumn', () => {
   it('should return true if the column is valid', () => {
-    const board = [      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    const board = [
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
       [2, 3, 4, 5, 6, 7, 8, 9, 1],
       [3, 4, 5, 6, 7, 8, 9, 1, 2],
       [4, 5, 6, 7, 8, 9, 1, 2, 3],
@@ -30,7 +32,8 @@ describe('checkColumn', () => {
   });
 
   it('should return false if the column is invalid', () => {
-    const board = [      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    const board = [
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
       [2, 3, 4, 5, 6, 7, 8, 9, 1],
       [3, 4, 5, 6, 7, 8, 9, 1, 2],
       [4, 5, 6, 7, 8, 9, 1, 2, 3],
@@ -43,4 +46,41 @@ describe('checkColumn', () => {
     const columnIndex = 0;
     expect(checkColumn(board, columnIndex)).toBe(false);
   });
+});
+
+test("checkSubgrid() returns true for valid subgrid", () => {
+  const grid = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    [2, 3, 4, 5, 6, 7, 8, 9, 1],
+    [5, 6, 7, 8, 9, 1, 2, 3, 4],
+    [8, 9, 1, 2, 3, 4, 5, 6, 7],
+    [3, 4, 5, 6, 7, 8, 9, 1, 2],
+    [6, 7, 8, 9, 1, 2, 3, 4, 5],
+    [9, 1, 2, 3, 4, 5, 6, 7, 8],
+  ];
+
+  const result = checkSubgrid(grid, 0, 0);
+  expect(result).toBe(true);
+});
+
+test("checkSubgrid() returns false for invalid subgrid", () => {
+  const grid = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    [2, 3, 4, 5, 6, 7, 8, 9, 1],
+    [5, 6, 7, 8, 9, 1, 2, 3, 4],
+    [8, 9, 1, 2, 3, 4, 5, 6, 7],
+    [3, 4, 5, 6, 7, 8, 9, 1, 2],
+    [6, 7, 8, 9, 1, 2, 3, 4, 5],
+    [9, 1, 2, 3, 4, 5, 6, 7, 8],
+  ];
+
+  // Change a value in the subgrid to create an invalid grid
+  grid[1][1] = 3;
+
+  const result = checkSubgrid(grid, 0, 0);
+  expect(result).toBe(false);
 });
