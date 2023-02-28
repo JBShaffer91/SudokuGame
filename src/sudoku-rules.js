1069250a683227655f29f5e9398e5784dc5eb471
Justin Shaffer <justinbshaffer91@gmail.com>
2023-02-27 20:32:25 -0800

    add passing test for checkBoard() function

--- src/sudoku-rules.js
+++ src/sudoku-rules.js
@@ -30,5 +30,38 @@ function checkSubgrid(grid, row, col, num) {
   return true;
 }
 
+function checkBoard(board) {
+  // Check each row
+  for (let row = 0; row < 9; row++) {
+    if (!checkRow(board, row)) {
+      return false;
+    }
+  }
 
-module.exports = { checkRow, checkColumn, checkSubgrid };
+  // Check each column
+  for (let col = 0; col < 9; col++) {
+    if (!checkColumn(board, col)) {
+      return false;
+    }
+  }
+
+  // Check each subgrid
+  for (let row = 0; row < 9; row += 3) {
+    for (let col = 0; col < 9; col += 3) {
+      if (!checkSubgrid(board, row, col)) {
+        return false;
+      }
+    }
+  }
+
+  // If all checks pass, the board is valid
+  return true;
+}
+
+
+module.exports = {
+  checkRow,
+  checkColumn,
+  checkSubgrid,
+  checkBoard
+};
--- test/sudoku-rules.test.js
+++ test/sudoku-rules.test.js
@@ -1,6 +1,7 @@
 const { checkRow } = require('../src/sudoku-rules');
 const { checkColumn } = require('../src/sudoku-rules');
 const { checkSubgrid } = require('../src/sudoku-rules');
+const { checkBoard } = require('../src/sudoku-rules');
 
 describe('checkRow', () => {
   it('should return true if the row is valid', () => {
@@ -82,5 +83,39 @@ test("checkSubgrid() returns false for invalid subgrid", () => {
   grid[1][1] = 3;
 
   const result = checkSubgrid(grid, 0, 0);
+  expect(result).toBe(false);
+});
+
+test("checkBoard() returns true for a valid board", () => {
+  const board = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9],
+  ];
+
+  const result = checkBoard(board);
+  expect(result).toBe(true);
+});
+
+test("checkBoard() returns false for an invalid board", () => {
+  const board = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 0, 2, 5, 6, 7],
+    [8, 5, 0, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 0, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 0, 0, 2, 8, 6, 1, 7, 9],
+  ];
+
+  const result = checkBoard(board);
   expect(result).toBe(false);
 });
