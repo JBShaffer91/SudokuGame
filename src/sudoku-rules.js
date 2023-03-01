382545a892a33c793f130c5ac51d860d7734e8c3
Justin Shaffer <justinbshaffer91@gmail.com>
2023-02-28 16:05:44 -0800

    create a SudokuPuzzle object in business logic

--- src/sudoku-rules.js
+++ src/sudoku-rules.js
@@ -1,66 +1,69 @@
-function checkRow(row) {
-  const set = new Set(row);
-  return set.size === row.length && !set.has(0);
-}
-
-function checkColumn(grid, columnIndex) {
-  const column = [];
-  for (let i = 0; i < grid.length; i++) {
-    column.push(grid[i][columnIndex]);
+class SudokuPuzzle {
+  constructor(board) {
+    this.board = board;
   }
-  const set = new Set(column);
-  return set.size === column.length && !set.has(0);
-}
 
-function checkSubgrid(grid, row, col, num) {
-  const subgridRowStart = Math.floor(row / 3) * 3;
-  const subgridColStart = Math.floor(col / 3) * 3;
-  const subgridValues = new Set();
+  checkRow(row) {
+    const set = new Set(row);
+    return set.size === row.length && !set.has(0);
+  }
 
-  for (let i = subgridRowStart; i < subgridRowStart + 3; i++) {
-    for (let j = subgridColStart; j < subgridColStart + 3; j++) {
-      const value = grid[i][j];
-      if (subgridValues.has(value)) {
-        return false;
+  checkColumn(columnIndex) {
+    const column = [];
+    for (let i = 0; i < this.board.length; i++) {
+      column.push(this.board[i][columnIndex]);
+    }
+    const set = new Set(column);
+    return set.size === column.length && !set.has(0);
+  }
+
+  checkSubgrid(row, col, num) {
+    const subgridRowStart = Math.floor(row / 3) * 3;
+    const subgridColStart = Math.floor(col / 3) * 3;
+    const subgridValues = new Set();
+
+    for (let i = subgridRowStart; i < subgridRowStart + 3; i++) {
+      for (let j = subgridColStart; j < subgridColStart + 3; j++) {
+        const value = this.board[i][j];
+        if (subgridValues.has(value)) {
+          return false;
+        }
+        subgridValues.add(value);
       }
-      subgridValues.add(value);
     }
+
+    return true;
   }
 
-  return true;
-}
-
-function checkBoard(board) {
-  // Check each row
-  for (let row = 0; row < 9; row++) {
-    if (!checkRow(board, row)) {
-      return false;
-    }
-  }
-
-  // Check each column
-  for (let col = 0; col < 9; col++) {
-    if (!checkColumn(board, col)) {
-      return false;
-    }
-  }
-
-  // Check each subgrid
-  for (let row = 0; row < 9; row += 3) {
-    for (let col = 0; col < 9; col += 3) {
-      if (!checkSubgrid(board, row, col)) {
+  checkBoard() {
+    // Check each row
+    for (let row = 0; row < 9; row++) {
+      if (!this.checkRow(this.board[row])) {
         return false;
       }
     }
-  }
 
-  // If all checks pass, the board is valid
-  return true;
+    // Check each column
+    for (let col = 0; col < 9; col++) {
+      if (!this.checkColumn(col)) {
+        return false;
+      }
+    }
+
+    // Check each subgrid
+    for (let row = 0; row < 9; row += 3) {
+      for (let col = 0; col < 9; col += 3) {
+        if (!this.checkSubgrid(row, col)) {
+          return false;
+        }
+      }
+    }
+
+    // If all checks pass, the board is valid
+    return true;
+  }
 }
 
 module.exports = {
-  checkRow,
-  checkColumn,
-  checkSubgrid,
-  checkBoard
+  SudokuPuzzle
 };
